c54f07b76864bf3264cf4b2c06b92e2a4d4ca0f6
AnsulaDev <ansulaoari88@gmail.com>
2023-05-04 22:37:19 +0530

    added moderation service to fliter comments and updated client/commentList

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,18 +10,25 @@ app.use(bodyParser.json());
 
 app.post('/events', (req, res) => {
     const event = req.body;
-    ///to send back events to post server
+    //to send back events to post server
     axios.post('http://localhost:4000/events', event).catch((err) =>{
         console.log(err.message);
     });
 
-    ////to send back event to comment server
+    //to send back event to comment server
     axios.post('http://localhost:5000/events', event).catch((err) => {
         console.log(err.message);
     });
+     //to send  event to query  server
     axios.post('http://localhost:7000/events', event).catch((err) => {
         console.log(err.message);
     });
+
+     //to send back event to moderation server
+    axios.post('http://localhost:8000/events', event).catch((err) => {
+        console.log(err.message);
+    });
+
 
 
     res.send({status: 'ok'});
---
+++ moderation-service/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const  cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res) => {
+    const {type, data} = req.body;
+    if(type==='CommentCreated'){
+        const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+        await axios.post('http://localhost:6000/events', {
+            type: 'CommentModerated',
+            data:{
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        });
+    }
+    res.send({});
+});
+
+app.listen(8000, () => {
+    console.log('running on port 8000');
+});
